b98970630543eac54f1f378f317453e1d89caf9c
samsul <samsul.marif100@gmail.com>
2023-04-25 08:19:06 +0800

    change permission and test create controller.go

--- app/Controllers/Controller.go
+++ app/Controllers/Controller.go
@@ -1,1 +1,16 @@
 package main
+
+import "github.com/gin-gonic/gin"
+
+func dummyJson(res *gin.Context) {
+	var arr []int
+	res.JSON(200, gin.H{
+		"message": append(arr, 1, 2, 3),
+	})
+}
+
+func main() {
+	req := gin.Default()
+	req.POST("/dummy", dummyJson)
+	req.Run()
+}
